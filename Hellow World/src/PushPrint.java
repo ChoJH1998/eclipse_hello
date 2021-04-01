5124364bc01140a73a163e8e6ceefa717063c7dd
ChoJH <chojh0208@naver.com>
2021-04-01 10:58:42 +0900

    Merge origin/master

--- Hellow World/src/PushPrint.java
+++ Hellow World/src/PushPrint.java
@@ -1,7 +1,7 @@
 
 public class PushPrint {
 	public static void main(String[] args) {
-		System.out.println(Add java file");
+		System.out.println("Add java file");
         System.out.println("Hello in Locale");
         System.out.println("Hello...");
 	}
